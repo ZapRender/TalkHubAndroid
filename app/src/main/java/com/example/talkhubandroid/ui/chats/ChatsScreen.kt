300f6c9863cc98461974550b8cb9140d26231ec2
Alejandro Zamudio <alejandro.zamudio@onoff.com.co>
2024-07-08 18:22:22 -0500

    Feat: Added top bar to chat screen

--- app/src/main/java/com/example/talkhubandroid/ui/chats/ChatsScreen.kt
+++ app/src/main/java/com/example/talkhubandroid/ui/chats/ChatsScreen.kt
@@ -1,17 +1,31 @@
 package com.example.talkhubandroid.ui.chats
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.talkhubandroid.ui.theme.Blue50
-import com.example.talkhubandroid.ui.theme.Blue600
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.talkhubandroid.R
+import com.example.talkhubandroid.ui.theme.Blue60
+import com.example.talkhubandroid.ui.theme.lightBlue
 
 @Composable
 @Preview(showSystemUi = true)
@@ -22,7 +36,7 @@ fun ChatsScreen() {
         }
     ) {innerPadding  ->
         LazyColumn(modifier = Modifier.padding(innerPadding)) {
-            //items()
+
         }
 
     }
@@ -31,8 +45,34 @@ fun ChatsScreen() {
 
 @Composable
 fun TopBarChats(){
-    Box(modifier = Modifier.fillMaxSize().background(color = Blue600)) {
-        //TopBar()
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = Blue60)) {
+
+        Row(modifier = Modifier.padding(top = 52.dp, start = 24.dp, end = 24.dp,bottom = 16.dp), horizontalArrangement = Arrangement.End) {
+            Icon(modifier = Modifier.padding(end = 10.dp), painter = painterResource(id = R.drawable.ic_chat_bubble), tint = lightBlue, contentDescription = "")
+            Box(modifier = Modifier.weight(1f), Alignment.CenterStart){
+                Text("E-Chat", color = Color.White, fontSize = 16.sp)
+            }
+            Icon(modifier = Modifier.padding(end = 16.dp), painter = painterResource(id = R.drawable.ic_search), tint = Color.White, contentDescription = "Search")
+            Icon(painter = painterResource(id = R.drawable.ic_add), tint = Color.White, contentDescription = "Search")
+        }
+
+
+    }
+}
+
+@Preview
+@Composable
+fun ChatItem(){
+    Box(){
+        Image(
+            modifier = Modifier.size(100.dp).clip(CircleShape),
+            painter = painterResource(id = R.drawable.ic_account_circle), contentDescription = "")
+        Column(){
+            Text("Name", fontSize = 16.sp)
+            Text("Message", fontSize = 14.sp)
+        }
 
     }
 }
